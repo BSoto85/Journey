92608fff2d6aed4dddef7e25f05ed47d4c31e1db
brenda-soto <bsoto@pursuit.org>
2024-06-05 13:46:15 -0400

    Components, pages and routes made

---
+++ src/Components/Detail.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import WhatIf from "./WhatIf";
+
+const Detail = () => {
+  return (
+    <div>
+      <WhatIf />
+    </div>
+  );
+};
+
+export default Detail;
---
+++ src/Pages/LandingPage.jsx
@@ -0,0 +1,11 @@
+import Home from "../Components/Home";
+
+const LandingPage = () => {
+  return (
+    <div>
+      <Home />
+    </div>
+  );
+};
+
+export default LandingPage;
